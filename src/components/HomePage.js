0d6ff3ee36bc7866086acb4e6ca7103888aa4bc6
AnasErr <anaserrahmadi@gmail.com>
2020-05-30 13:22:47 +0200

    add infinite scroll

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from 'react'
-import { Container , AppBar,Toolbar, IconButton, Typography} from '@material-ui/core';
+import React, { useState, useEffect, useRef, useCallback} from 'react'
+import { Container , AppBar,Toolbar, IconButton, Typography, Box} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import HomeIcon from '@material-ui/icons/Home';
 import axios from 'axios'
@@ -9,7 +9,7 @@ import MovieItem from './MovieItem'
 
 const useStyles = makeStyles((theme) => ({
     header: {
-      flexGrow: 1,
+      
     },
     title: {
       flexGrow: 1,
@@ -20,8 +20,7 @@ const useStyles = makeStyles((theme) => ({
     },
     main: {
         flexGrow: 1,
-        marginTop: '20px',
-        minHeight: '70vh'
+        marginTop: '10px',
     },
   }));
 
@@ -31,13 +30,28 @@ const HomePage = () => {
     const [movies, setMovies] = useState([])
     const [pageNumber, setPageNumber] = useState(1)
     const [loading, setLoading] = useState(true)
+    const [hasNext, setHasNext] = useState(true)
+
+    const observer = useRef()
+    const lastElement = useCallback(node => {
+        if(loading) return
+        if(observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if(entries[0].isIntersecting && hasNext){
+                console.log('visible')
+                setPageNumber(prevPage => prevPage + 1 )
+            }
+        })
+        if (node) observer.current.observe(node)
+    }, [loading])
 
     useEffect(() => {
+        setLoading(true)
         fetchData()
         return () => {
             console.log('done');
         }
-    }, []);
+    }, [pageNumber]);
 
     // get data from api endpoint
     const fetchData = () => {
@@ -48,20 +62,30 @@ const HomePage = () => {
             }
         })
         .then(res => {
-            res.data.results.map(item => {
-                item.image = `${IMAGE_BASE_URL}w780${item.poster_path}`
-                console.log(item)
-                return item
-            })
-            setMovies([...res.data.results])
-            console.log(res.data.results)
+            if(res.data && res.data.results){
+                res.data.results.map(item => {
+                    item.image = `${IMAGE_BASE_URL}w780${item.poster_path}`
+                    return item
+                })
+                setMovies(prevMovies => {
+                    return [...prevMovies, ...res.data.results]
+                })
+                if(res.data.total_pages > pageNumber){
+                    setHasNext(true)
+                }else {
+                    setHasNext(false)
+                }
+                setLoading(false)
+                console.log(res.data.results)
+            }
+            
         })
         .catch(error => console.log(error))
         
     }
 
     return (
-        <React.Fragment>
+        <div style={{backgroundColor: '#e1c919'}}>
             <header className={classes.header}>
                 <AppBar position="static" className={classes.appBar}>
                     <Toolbar>
@@ -73,9 +97,14 @@ const HomePage = () => {
                 </AppBar>
             </header>
             <Container maxWidth="md" className={classes.main}>
-                {movies.map(item => <MovieItem key={item.id} movie={item} /> )}
+                {movies.map((item, index) => {
+                    return (movies.length === index + 1) ? 
+                        <MovieItem key={item.id} movie={item} ref={lastElement}/> :
+                        <MovieItem key={item.id} movie={item} />
+                })}
+                {loading && <Box justifyContent="center"><img src="https://media.giphy.com/media/y1ZBcOGOOtlpC/source.gif"/></Box>}
             </Container>
-        </React.Fragment>
+        </div>
     );
 }
  
--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,50 +1,59 @@
 import React from 'react'
-import { ListItem , ListItemAvatar,ListItemText, Avatar, Typography} from '@material-ui/core';
+import { ListItem , ListItemAvatar,ListItemText, Avatar, Typography, Grid} from '@material-ui/core';
 import ImageIcon from '@material-ui/icons/Image';
+import StarIcon from '@material-ui/icons/Star';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     image: {
-        width: '25vh',
-        height: '20vw',
+        width: 'auto',
+        height: '60vh',
         marginRight: '10px'
     },
+    item: {
+        backgroundColor: '#1e1d1d',
+        fontFamily: 'Verdana',
+        margin: '10px 0',
+        borderRadius: '5px',
+        color: '#ffffff'
+    },
+    title: {
+        fontSize: '40px'
+    },
+    date: {
+        fontSize: '20px',
+        color: 'grey',
+        fontStyle: 'italic',
+        margin: '-1px 0 10px'
+    }
 }));
 
-function MovieItem({movie}) {
+const MovieItem = React.forwardRef(({movie}, ref) =>  {
     const classes = useStyles();
     return(
-        <ListItem alignItems="flex-start" >
-            <ListItemAvatar>
-                {movie.image ? <Avatar variant="rounded" src={movie.image} className={classes.image} /> :
-                    <Avatar variant="rounded" className={classes.image}>
-                        <ImageIcon />
-                    </Avatar>
-                }
-            </ListItemAvatar>
-            <ListItemText
-                primary={
-                    <div>
-                        <Typography 
-                            component="h5"
-                            variant="h5"
-                            color="textPrimary"
-                        >
-                            {movie.title}
-                        </Typography>
-                        <Typography
-                            component="span"
-                            variant="h6"
-                            color="textPrimary"
-                        >
-                            {`Date de sortie : ${new Date(`${movie.release_date}`).getFullYear()}`}
-                            
-                        </Typography>
+        <Grid container spacing={4} className={classes.item}>
+                <Grid item xs={12} sm={4}>
+                    {movie.image ? <Avatar variant="rounded" src={movie.image} className={classes.image} /> :
+                        <Avatar variant="rounded" className={classes.image}>
+                            <ImageIcon />
+                        </Avatar>
+                    }
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <div className={classes.title}>{movie.title}</div>
+                    <div className={classes.date}>
+                        {new Date(`${movie.release_date}`).getFullYear()}
                     </div>
-                }
-            />
-        </ListItem>
+                    <div ref={ref} style={{fontSize: '30px'}}>
+                        {`${movie.vote_average}`}
+                        <StarIcon style={{color: '#eed522', fontSize: '40px', marginBottom: '-7px'}} />
+                    </div>
+                    {movie.overview && <p> {movie.overview} </p>}
+                </Grid>
+            </Grid>
+            
+    
     )
-}
+})
  
-export default MovieItem;
+export default React.memo(MovieItem);
